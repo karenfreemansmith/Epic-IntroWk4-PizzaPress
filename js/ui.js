512e44a1e5b2ca2989c8e193d64098e4f384b6c4
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-08-27 20:19:35 -0700

    add base pizza price

--- js/ui.js
+++ js/ui.js
@@ -1,10 +1,16 @@
 $(".businessName").text(Business.businessName);
 $("title").text(Business.businessName);
 $("#newBusinessName").val(Business.businessName);
+$("#newBasePrice").val(Business.basePrice);
 
 $("#editBusinessName button.update").click(function() {
   Business.businessName=$("#newBusinessName").val();
   saveBusiness(Business);
   $(".businessName").text(Business.businessName);
   $("title").text(Business.businessName);
+});
+
+$("#editBasePrice button.update").click(function() {
+  Business.basePrice=$("#newBasePrice").val();
+  saveBusiness(Business);
 });
